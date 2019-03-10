b048df1ca1a64ccb39434d905474109aae704c17
dahaekim88 <dhkim1014@gmail.com>
2019-03-10 21:40:15 +0900

    Use page query to get markdown data

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 import Header from "../../components/header"
 
 import styles from "./blog.module.css"
+import jsThumbnail from "../../images/javascript.png"
 
 const Post = props => (
   <div className={styles.post}>
@@ -11,9 +13,11 @@ const Post = props => (
       <img src={props.thumbnail} className={styles.thumbnail} alt="" />
       <div className={styles.description}>
         <h2 className={styles.title}>{props.title}</h2>
-        <p className={styles.excerpt}>{props.excerpt}</p>
+        <p>{props.date}</p>
+        {/* tags */}
       </div>
     </div>
+    <p className={styles.excerpt}>{props.excerpt}</p>
   </div>
 )
 
@@ -22,16 +26,37 @@ export default ({ data }) => {
   return (
     <Layout>
       <Header headerText="Blog" />
-      <Post
-        title="Jane Doe"
-        thumbnail="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-      />
-      <Post
-        title="Bob Smith"
-        thumbnail="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-      />
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <Post
+          key={node.id}
+          thumbnail={jsThumbnail}
+          title={node.frontmatter.title}
+          date={node.frontmatter.date}
+          excerpt={node.excerpt}
+        />
+      ))}
+      {/* pagination */}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 5
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
