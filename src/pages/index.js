c6ce8b260f933638f72eb24929668e928e3444a1
dahaekim88 <dhkim1014@gmail.com>
2019-03-15 20:22:57 +0900

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,31 @@
 import React from "react"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import Header from "../components/header"
+import Bio from "../components/bio"
+import { NavMenu } from "../components/navmenu"
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const NavBar = styled.div`
+  margin: 0 auto;
+`
 
 export default () => (
-  <Layout>
-    <Header headerText="Home" />
-  </Layout>
+  <Container>
+    <Bio />
+    <NavBar>
+      <NavMenu to="/blog" padding="0.5rem">
+        Blog
+      </NavMenu>
+      <NavMenu to="/projects" padding="0.5rem">
+        Projects
+      </NavMenu>
+    </NavBar>
+  </Container>
 )
