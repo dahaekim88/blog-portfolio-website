bad059cc715dbea30da68a6142bd8af845c80dce
dahaekim88 <dhkim1014@gmail.com>
2019-03-15 09:48:47 +0900

    Update config to fix error

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ module.exports = {
     title: `Dahae Kim`,
   },
   plugins: [
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/img`,
-        name: "uploads",
-      },
-    },
+    // {
+    //   // keep as first gatsby-source-filesystem plugin for gatsby image support
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     path: `${__dirname}/static/img`,
+    //     name: "uploads",
+    //   },
+    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
