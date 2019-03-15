58adb3f09c74ec167d434edc7ca84f258f324d31
dahaekim88 <dhkim1014@gmail.com>
2019-03-15 20:22:13 +0900

    Update layout with sidebar and add helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,98 @@
 import React from "react"
+import styled from "styled-components"
 import { Helmet } from "react-helmet"
-import { Link, StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
-import layoutStyles from "./layout.module.css"
+import Bio from "./bio"
+import { NavMenu } from "./navmenu"
 
-const NavMenu = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+const Layout = styled.div``
+
+const SideBar = styled.div`
+  height: 100vh;
+  width: 300px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  flex: 300 0;
+  @media screen and (max-width: 720px) {
+    display: none;
+  }
+`
+
+const Footer = styled.div`
+  width: 100%;
+  font-size: 0.8rem;
+  text-align: center;
+  margin: 1rem auto;
+`
+
+const Container = styled.div`
+  padding: 2rem;
+  position: relative;
+  max-width: 45rem;
+
+  @media screen and (min-width: 720px) {
+    margin-left: 300px;
+  }
+`
+
+const Main = styled.div``
 
 export default ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div className={layoutStyles.container}>
-        <Helmet>
-          <html lang="ko" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta
-            name="description"
-            content={data.site.siteMetadata.description}
-          />
-          <meta property="og:url" content="/" />
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          {/* <meta property="og:image" content="/img/og-image.jpg" /> */}
-        </Helmet>
-        <header>
-          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-            <h3 style={{ display: `inline` }}>
-              {data.site.siteMetadata.title}
-            </h3>
-          </Link>
-          <ul style={{ listStyle: `none`, float: `right` }}>
-            <NavMenu to="/blog">Blog</NavMenu>
-            <NavMenu to="/projects">Projects</NavMenu>
-          </ul>
-        </header>
-        {children}
-      </div>
-    )}
+    query={query}
+    render={data => {
+      const { title, description } = data.site.siteMetadata
+      return (
+        <Layout>
+          <Helmet>
+            <html lang="ko" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta property="og:url" content="/" />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content={title} />
+            {/* <meta property="og:image" content="/img/og-image.jpg" /> */}
+          </Helmet>
+          <SideBar>
+            <Bio />
+            <Footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </Footer>
+          </SideBar>
+          <Container>
+            <NavMenu to="/" padding="0">
+              Home
+            </NavMenu>
+            <NavMenu to="/blog" padding="0.5rem">
+              Blog
+            </NavMenu>
+            <NavMenu to="/projects" padding="0.5rem">
+              Projects
+            </NavMenu>
+            <Main>{children}</Main>
+          </Container>
+        </Layout>
+      )
+    }}
   />
 )
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
