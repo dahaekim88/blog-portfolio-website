d1c1fecf9f8565b1d510589631266fae1f8fae66
dahaekim88 <dhkim1014@gmail.com>
2019-03-15 20:26:46 +0900

    Create new page, component, template for tags

---
+++ src/components/tag.js
@@ -0,0 +1,28 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const TagLink = styled(Link)`
+  font-size: 0.8rem;
+  color: #ffbf00;
+  text-decoration: none;
+`
+
+export default ({ tags }) => {
+  return (
+    <>
+      {tags.map((tag, index) => {
+        let separator = ", "
+        if (index + 1 === tags.length) {
+          separator = ""
+        }
+        return (
+          <span key={tag}>
+            <TagLink to={`tags/${tag}`}>{tag}</TagLink>
+            {separator}
+          </span>
+        )
+      })}
+    </>
+  )
+}
