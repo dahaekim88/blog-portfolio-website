ef1ed7a7b511615f4d65bc448396eb15a6ac6cac
dahaekim88 <dhkim1014@gmail.com>
2019-03-31 19:33:45 +0900

    Minor css edit

--- src/components/title.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styled from "styled-components"
-
-const PageTitle = styled.h1`
-  color: #1ca086;
-`
-
-export default ({ children }) => <PageTitle>{children}</PageTitle>
