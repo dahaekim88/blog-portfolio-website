5718ab87b09003ee4987479ef6f5c3f3fb939c06
dahaekim88 <dhkim1014@gmail.com>
2019-04-02 12:58:42 +0900

    Add apple-touch-icon

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,14 +1,17 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
+import icon from "../images/icon.png"
+
 export default ({ title, description }) => (
   <Helmet>
     <html lang="ko" />
     <title>{title}</title>
     <meta name="description" content={description} />
+    <link rel="apple-touch-icon" sizes="180x180" href={icon} />
     <meta property="og:url" content="/" />
     <meta property="og:type" content="article" />
     <meta property="og:title" content={title} />
-    {/* <meta property="og:image" content="/img/og-image.jpg" /> */}
+    <meta property="og:image" content={icon} />
   </Helmet>
 )
