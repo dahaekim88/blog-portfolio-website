8de26b27f6716f5a1716767371aef9b1c2605bf0
dahaekim88 <dhkim1014@gmail.com>
2019-04-06 23:30:30 +0900

    Add animated mouse scroll icon

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,12 +6,12 @@ import { NavMenu } from "./navmenu"
 
 const SideBar = styled.div`
   background-color: #3c2e3d; // 735b69
-  height: 100%;
-  width: 35%;
   position: fixed;
-  z-index: 1;
   top: 0;
   left: 0;
+  min-width: 35%;
+  min-height: 100%;
+  z-index: 1;
   overflow-x: hidden;
   display: flex;
   justify-content: center;
@@ -23,12 +23,8 @@ const SideBar = styled.div`
     position: relative;
     text-align: center;
     top: auto;
-    width: 100%;
-    display: block;
-  }
-
-  @media screen and (max-width: 600px) {
-    padding: 4em 4em;
+    min-width: 100%;
+    min-height: 100vh;
   }
 `
 
@@ -44,12 +40,81 @@ const Footer = styled.div`
   font-size: 0.8rem;
   text-align: center;
   margin: 1rem auto;
-  position: relative;
-  top: 5rem;
   color: #fff;
+`
+const Mouse = styled.div`
+  @media screen and (max-width: 980px) {
+    position: absolute;
+    width: 22px;
+    height: 42px;
+    bottom: 40px;
+    left: 50%;
+    margin-left: -12px;
+    border-radius: 15px;
+    border: 2px solid #f1c3d3;
+    -webkit-animation: intro 1s;
+    animation: intro 1s;
 
-  @media screen and (max-width: 600px) {
-    top: 2rem;
+    @-webkit-keyframes intro {
+      0% {
+        opacity: 0;
+        -webkit-transform: translateY(40px);
+        transform: translateY(40px);
+      }
+      100% {
+        opacity: 1;
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+      }
+    }
+
+    @keyframes intro {
+      0% {
+        opacity: 0;
+        -webkit-transform: translateY(40px);
+        transform: translateY(40px);
+      }
+      100% {
+        opacity: 1;
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+      }
+    }
+  }
+`
+
+const Scroll = styled.div`
+  @media screen and (max-width: 980px) {
+    display: block;
+    width: 3px;
+    height: 3px;
+    margin: 6px auto;
+    border-radius: 4px;
+    background: #f1c3d3;
+    -webkit-animation: finger 2s infinite;
+    animation: finger 2s infinite;
+
+    @-webkit-keyframes finger {
+      0% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0;
+        -webkit-transform: translateY(20px);
+        transform: translateY(20px);
+      }
+    }
+
+    @keyframes finger {
+      0% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0;
+        -webkit-transform: translateY(20px);
+        transform: translateY(20px);
+      }
+    }
   }
 `
 
@@ -68,9 +133,12 @@ export default () => (
       </NavMenu>
     </MenuContainer>
     <Footer>
-      © {new Date().getFullYear()}, Built with
+      © dada {new Date().getFullYear()}, Built with
       {` `}
       <a href="https://www.gatsbyjs.org">Gatsby</a>
     </Footer>
+    <Mouse>
+      <Scroll />
+    </Mouse>
   </SideBar>
 )
