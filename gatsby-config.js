e88060c0c7e71496a2420b1199d4a493f98d454c
dahaekim88 <dhkim1014@gmail.com>
2019-04-13 00:04:04 +0900

    Update config and dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-sass`,
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
