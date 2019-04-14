dd27ae51e4a79aa959ac23d84214b218f15f3a61
dahaekim88 <dhkim1014@gmail.com>
2019-04-14 16:00:41 +0900

    Add tooltip for sns link button

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
+import ReactTooltip from "react-tooltip"
 
-import { Container, StyledHref } from "../components/reusable"
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"
+import { Center, StyledHref, Sns } from "../components/reusable"
+import {
+  IoLogoGithub,
+  IoLogoLinkedin,
+  IoMdMail,
+  IoMdCopy,
+} from "react-icons/io"
 import profile from "../images/icon.png"
 
-const Sns = styled.div`
-  display: inline-block;
-  margin: 0.45rem;
+const Paragraph = styled.p`
+  text-align: center;
+  padding: 20px;
+  color: #333;
 `
 
 export default () => (
@@ -18,21 +25,25 @@ export default () => (
       const { social } = data.site.siteMetadata
 
       return (
-        <Container>
+        <Center direction="column">
           <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
             <img src={profile} alt="dada" style={{ borderRadius: "50%" }} />
           </Link>
-          {/* <h2 style={{ marginTop: `1rem` }}>{title}</h2>
-          <p>Web Developer</p> */}
+          <Paragraph>Former Fashion Buyer, Now Web Developer</Paragraph>
           <div>
             <Sns>
               <StyledHref
                 href={`https://github.com/${social.github}`}
                 rel="noopener noreferrer"
                 target="_blank"
-                color="#fff"
+                color="#ecc7c0"
+                data-tip
+                data-for="github"
               >
-                <FaGithub />
+                <ReactTooltip id="github" place="bottom" effect="solid">
+                  <span>Github</span>
+                </ReactTooltip>
+                <IoLogoGithub />
               </StyledHref>
             </Sns>
             <Sns>
@@ -40,18 +51,46 @@ export default () => (
                 href={`https://linkedin.com/in/${social.linkedin}`}
                 rel="noopener noreferrer"
                 target="_blank"
-                color="#fff"
+                color="#ecc7c0"
+                data-tip
+                data-for="linkedin"
               >
-                <FaLinkedin />
+                <ReactTooltip id="linkedin" place="bottom" effect="solid">
+                  <span>LinkedIn</span>
+                </ReactTooltip>
+                <IoLogoLinkedin />
               </StyledHref>
             </Sns>
             <Sns>
-              <StyledHref href={`mailto:${social.email}`} color="#fff">
-                <FaEnvelope />
+              <StyledHref
+                href={`mailto:${social.email}`}
+                color="#ecc7c0"
+                data-tip
+                data-for="gmail"
+              >
+                <ReactTooltip id="gmail" place="bottom" effect="solid">
+                  <span>Gmail</span>
+                </ReactTooltip>
+                <IoMdMail />
+              </StyledHref>
+            </Sns>
+            <Sns>
+              <StyledHref
+                href={`https://docs.google.com/document/d/18XZhwGPPwZs2Lqt8khXxLTvMKjQcpWzo_t8CmMD-noI/edit?usp=sharing`}
+                rel="noopener noreferrer"
+                target="_blank"
+                color="#ecc7c0"
+                data-tip
+                data-for="resume"
+              >
+                <ReactTooltip id="resume" place="bottom" effect="solid">
+                  <span>Résumé</span>
+                </ReactTooltip>
+                <IoMdCopy />
               </StyledHref>
             </Sns>
           </div>
-        </Container>
+        </Center>
       )
     }}
   />
