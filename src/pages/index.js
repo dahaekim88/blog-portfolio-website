bcbca108825d9db70df243b81fe3f8bce12bb21c
dahaekim88 <dhkim1014@gmail.com>
2019-04-14 21:59:58 +0900

    Update index page style

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import Header from "../components/header"
+import Bio from "../components/bio"
 import Post from "../components/post"
 import Project from "../components/project"
 import {
+  Container,
+  Center,
   StyledButton,
-  StyledHr,
   Row,
   Col,
-  PageTitle,
 } from "../components/reusable"
 
 export default ({ data }) => {
@@ -30,70 +32,91 @@ export default ({ data }) => {
 
   return (
     <Layout>
-      <section id="one">
-        <PageTitle>Recent Post</PageTitle>
-        <Post
-          key={post.id}
-          title={post.frontmatter.title}
-          date={post.frontmatter.date}
-          description={post.frontmatter.description}
-          excerpt={post.excerpt}
-          to={post.fields.slug}
-          tags={post.frontmatter.tags}
-        />
-        <Link to={post.fields.slug}>
-          <StyledButton>Read More</StyledButton>
-        </Link>
+      <section id="bio">
+        <Container>
+          <Header title="About Me" />
+          <Bio />
+        </Container>
       </section>
 
-      <StyledHr />
-
-      <section id="two">
-        <PageTitle>Recent Projects</PageTitle>
-        <Row>
-          {projects.map(({ node }) => (
-            <Col>
-              <Project
-                key={node.id}
-                image={node.frontmatter.image}
-                title={node.frontmatter.title}
-                to={node.fields.slug}
-              />
-            </Col>
-          ))}
-        </Row>
-        <Link to="/projects">
-          <StyledButton>Full Portfolio</StyledButton>
-        </Link>
+      <section id="post">
+        <Container>
+          <Header title="Recent Post" />
+          <Post
+            key={post.id}
+            title={post.frontmatter.title}
+            date={post.frontmatter.date}
+            description={post.frontmatter.description}
+            excerpt={post.excerpt}
+            to={post.fields.slug}
+            tags={post.frontmatter.tags}
+          />
+          <Link to={post.fields.slug}>
+            <StyledButton>Read More</StyledButton>
+          </Link>
+        </Container>
       </section>
 
-      <StyledHr />
-
-      <section id="three">
-        <PageTitle>Get In Touch</PageTitle>
-        <form method="post" action="mailto:dhkim1014@gmail.com">
+      <section id="projects">
+        <Container>
+          <Header title="Recent Projects" />
           <Row>
-            <Col>
-              <input type="text" name="name" id="name" placeholder="Name" />
-            </Col>
-            <Col>
-              <input type="email" name="email" id="email" placeholder="Email" />
-            </Col>
+            {projects.map(({ node }) => (
+              <Col>
+                <Project
+                  key={node.id}
+                  image={node.frontmatter.image}
+                  title={node.frontmatter.title}
+                  tags={node.frontmatter.tags}
+                  to={node.fields.slug}
+                  titleSize="1.5rem"
+                  tagSize="0.8rem"
+                />
+              </Col>
+            ))}
           </Row>
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            placeholder="Subject"
-          />
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            rows="4"
-          />
-          <StyledButton type="submit">Send Message</StyledButton>
-        </form>
+          <Center>
+            <Link to="/projects">
+              <StyledButton>Full Portfolio</StyledButton>
+            </Link>
+          </Center>
+        </Container>
+      </section>
+
+      <section id="contact">
+        <Container>
+          <Header title="Get In Touch" />
+          <form method="post" action="mailto:dhkim1014@gmail.com">
+            <Row>
+              <Col>
+                <input type="text" name="name" id="name" placeholder="Name" />
+              </Col>
+              <Col>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                />
+              </Col>
+            </Row>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              placeholder="Subject"
+            />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message"
+              rows="4"
+            />
+            <Center>
+              <StyledButton type="submit">Send Message</StyledButton>
+            </Center>
+          </form>
+        </Container>
       </section>
     </Layout>
   )
