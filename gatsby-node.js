a9dd495934621d55b4f7a15f53547e533663c488
dahaekim88 <dhkim1014@gmail.com>
2019-04-19 16:05:49 +0900

    Create tag slug for projects

--- gatsby-node.js
+++ gatsby-node.js
@@ -83,7 +83,7 @@ exports.createPages = ({ graphql, actions }) => {
 
     let tags = []
     // Iterate through each post, putting all found tags into `tags`
-    _.each(posts, edge => {
+    _.each(results, edge => {
       if (_.get(edge, "node.frontmatter.tags")) {
         tags = tags.concat(edge.node.frontmatter.tags)
       }
