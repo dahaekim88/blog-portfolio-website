8e2717d8c83667196bb70a5a137c7c31f042e3e4
Kim Dahae <dhkim1014@gmail.com>
2020-02-10 12:33:33 +0900

    Update remark theme & styles for inline code

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,6 +82,11 @@ module.exports = {
               // If setting this to true, the parser won't handle and highlight inline
               // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: false,
+              colorTheme: 'Night Owl',
+              extensions: [{
+                identifier: 'sdras.night-owl',
+                version: '1.1.3'
+              }],
             },
           },
         ],
